9c64c7a48903f30160d0c71872cda66d0ef0b05e
Kafui Mortey <morteykafui@gmail.com>
2024-07-03 15:57:16 +0000

    add kinde authentication

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,75 @@
+import DashboardNavigation from "@/components/DashboardNavigation";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { CircleUser, MenuIcon } from "lucide-react";
+import { redirect } from "next/navigation";
+import { PropsWithChildren } from "react";
+
+const DashboardLayout = async ({ children }: PropsWithChildren) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user.email !== "kafuidev@gmail.com") {
+    return redirect("/");
+  }
+
+  return (
+    <div className="flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <header className="sticky top-0 flex h-16 justify-between items-center border-b gap-4 bg-white">
+        <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm">
+          <DashboardNavigation />
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              className="shrink-0 md:hidden"
+              variant={"outline"}
+              size={"icon"}
+            >
+              <MenuIcon className="size-5" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <nav className="grid gap-6 text-lg font-medium mt-8">
+              <DashboardNavigation />
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant={"secondary"}
+              size={"icon"}
+              className="rounded-full"
+            >
+              <CircleUser className="size-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Button asChild>
+                <LogoutLink>Logout</LogoutLink>
+              </Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
